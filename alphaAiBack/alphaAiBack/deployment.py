b5366a3f2af231d086a4be4a01766a81d3c7ce62
Poorvi <jainpoorvi49@gmail.com>
2024-12-07 20:49:47 +0530

    add env data

--- alphaAiBack/alphaAiBack/deployment.py
+++ alphaAiBack/alphaAiBack/deployment.py
@@ -71,64 +71,16 @@ CONNECTION_STR = os.environ.get('AZURE_SQL_CONNECTIONSTRING')
 if not CONNECTION_STR:
     raise ValueError("AZURE_SQL_CONNECTIONSTRING is not set in environment variables.")
 
-# Parse the connection string
-# connection_dict = {}
-# for part in CONNECTION_STR.split(';'):
-#     if '=' in part:
-#         key, value = part.split('=', 1)
-#         connection_dict[key.strip()] = value.strip()
 
-# # Extract HOST and PORT from the connection string
-# host_port_part = CONNECTION_STR.split('://')[-1].split(';')[0]
-# host, port = host_port_part.split(':') if ':' in host_port_part else (host_port_part, '1433')
-
-# print("==================host",host)
-# print("================>>>>>>>>>", host_port_part)
-
-# # Database configuration
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'sql_server.pyodbc',
-#         'NAME': connection_dict.get('database'),
-#         'HOST': host,
-#         'PORT': port,
-#         'USER': connection_dict.get('user'),
-#         'PASSWORD': connection_dict.get('password'),
-#         'OPTIONS': {
-#             'driver': 'ODBC Driver 18 for SQL Server',
-#             'encrypt': 'no',
-#             'trustServerCertificate': 'yes',
-#             'timeout': 60,
-#         },
-#     }
-# }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'sql_server.pyodbc',
-#         'NAME': 'alphaai',  # Your database name
-#         'USER': 'alphaadmin',  # Your database username
-#         'PASSWORD': 'Alphaaifund@123',  # Your password
-#         'HOST': 'alphaaisql.database.windows.net',  # Your SQL Server host
-#         'PORT': '1433',  # Default port for SQL Server
-#         'OPTIONS': {
-#             'driver': 'ODBC Driver 18 for SQL Server',  # ODBC driver for SQL Server
-#             'encrypt': 'yes',  # Encrypt the connection
-#             'trustServerCertificate': 'no',  # Don't trust server certificate, use proper SSL certificates
-#             'Connection Timeout': 30,  # Connection timeout in seconds
-#         },
-#     }
-# }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'AlphaAi',
-        'USER': 'root',
-        'PASSWORD': 'Jain@123',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'USER': 'rgdtfwming',
+        'PASSWORD': '$0BuDlj$Yf5IlByq',
+        'HOST':'alpha-ai-dashboard-server.postgres.database.azure.com',
+        'PORT':'5432',
     }
 }
 
